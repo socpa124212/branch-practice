3c17652f33fce545ba994307744c94315dae5fc7
socpa124212 <socpa1234@kau.kr>
2023-02-03 14:25:42 +0900

    feat : add elif to print buzz
    condition : if i is muliple of 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,5 +8,7 @@ user_num = int(input('Hello, there! Enter some positive integer: '))
 for i in range(1, user_num+1):
     if i%3 == 0 :
         print('fizz')
+    elif i%5 == 0:
+        print('buzz')
     else:
         print(f'{i}')
