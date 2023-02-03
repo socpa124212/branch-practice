d4321bcfd9eae058d455ac57c4fd6c748ae27fc6
socpa124212 <socpa1234@kau.kr>
2023-02-03 14:19:18 +0900

    feat : create fizzbuzz.py, getting user_num

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+# get integer from user
+user_num = int(input('Hello, there! Enter some positive integer: '))
+
+# print user_num for debugging
+print(user_num)
+
+# Iteration
+for i in range(1, user_num+1):
+    print(i)
